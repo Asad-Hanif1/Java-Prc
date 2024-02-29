c9ded0af8cc230365d6f7d31c78008ead00fcfb2
Asad-Hanif1 <hanifasad617@gmail.com>
2024-02-29 20:04:52 +0500

    index file created

---
+++ JS If else assigiment/Question 11/app.js
@@ -0,0 +1,8 @@
+var firstName = "Asad";
+
+
+if (firstName === "Fahad") {
+    document.write("Hello Danish!");
+   }else{
+    console.log("You Are Not Danish")
+   }
---
+++ JS If else assigiment/Question 4/app.js
@@ -0,0 +1,8 @@
+var userName = prompt("Enter Your Correct Name ")
+var myName = ("Asad Hanif") 
+
+if( userName == myName ){
+    console.log("Hi, ThankYou For Visting Our Website")
+}else(
+    console.log()
+)
