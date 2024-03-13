4371105b2aa0e1a57b5a72e071d3f56efac612f4
Asad-Hanif1 <hanifasad617@gmail.com>
2024-03-13 15:08:12 +0500

    loops created file

---
+++ Loops/main.js
@@ -0,0 +1,189 @@
+// console.log(`hello asad hanif `);
+////////intro
+// console.log('hello world')
+
+
+//forLoop
+//while loop
+//do while loop
+
+
+
+// for(let i = 1; i <= 10; i++){
+//     console.log('hello world' , i);
+
+
+//     // i++
+// }
+// console.log('loop ka bahar');
+
+///////////////////////////////////////-------------------------------------------------------------
+
+// const userInput = +prompt('enter table number')
+// const times = +prompt('How many time do you want?')
+
+// for (let i = 1; i <= times; i++) {
+//     console.log(`${userInput} * ${i} = ${userInput*i}`);
+// }
+
+////////////////////------------------------------------------------------/////////////////////////////
+
+//infinite loop
+// for (let i = 1; i > 0; i++) {
+//     console.log(i);
+// }
+///////////////////--------------------------------------------------////////////////////////
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log('outer Loop ===> ' , i);
+//     for(let j = 1; j <= 5; j++){
+//         console.log('inner Loop===>' , j);
+//     }
+// }
+
+///////-----------------------------/////////////////////////////
+
+// for (let i = 1; i <= 100; i++) {
+//     // console.log(i);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+
+
+// let fruits = ['apple' , 'banana' , 'orange' , 'watermelon' , 'grapes'];
+
+// console.log(fruits);
+// const ul = document.querySelector('ul');
+// for (let i = 0; i < fruits.length; i++) {
+//     // console.log(fruits[i]);
+//     ul.innerHTML += `<li>${fruits[i]}</li>`
+// }
+
+
+///////---------------------------------------/////////////////////////////
+//////Question no 1
+
+
+// for (let i = 0; i < 5; i++) {
+//     document.write("Hello World <br>");
+//   }
+
+
+
+//////Question no 2
+
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+
+//////Question no 3
+
+// Taking input for the table number and length
+const tableNumber = parseInt(prompt("Enter the number for the multiplication table:"));
+const tableLength = parseInt(prompt("Enter the length of the multiplication table:"));
+
+// Checking if the input is valid
+if (isNaN(tableNumber) || isNaN(tableLength)) {
+    console.log("Invalid input. Please enter valid numbers.");
+} else {
+    console.log(`Multiplication table for ${tableNumber} up to ${tableLength}:`);
+    for (let i = 1; i <= tableLength; i++) {
+        console.log(`${tableNumber} x ${i} = ${tableNumber * i}`);
+    }
+}
+
+
+//////Question no 4
+
+const A = ["Apple", "samosa", "Chicken", "Jucie", "Cold drink"];
+
+for (let i = 0; i < A.length; i++) {
+    console.log(A[i]);
+}
+
+
+
+
+
+//////Question no 5
+
+const fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+
+
+
+
+
+//////Question no 1
+
+
+
+//////Question no 1
+
+
+
+//////Question no 1
+
+
+
+
+
+//////Question no 1
+
+
+
+
+//////Question no 1
+
+
+
+
+
+//////Question no 1
+
+
+
+
+//////Question no 1
+
+
+
+
+//////Question no 1
+
+
+
+
+//////Question no 1
+
+
+
+
+//////Question no 1
+
+
+
+//////Question no 1
+
+
+
+//////Question no 1
+
+
+
+
+
+
+//////Question no 1
+
+
+
+
--- arry/index.js
+++ arry/index.js
@@ -124,3 +124,63 @@ console.log(newArr);
 // const names = ['Asad' , 'Hanif' , 'DH'];
 // const str = names.join(' ')
 // console.log(str);
+
+
+
+
+
+
+
+//concat
+//sort
+//reverse
+//indexof
+
+
+
+// const shop = ['Apple', 'Banana',  'Orange', 'Mango', 'Grape', 'Strawberry']
+// console.log(fruits.includes('dafdsfdsfd'));
+// console.log(fruits.indexOf('fsdkjfhsdjkfh'));
+
+// fruits.shift();
+// fruits.unshift('Guaava' , 'Faalsa');
+// fruits.unshift('Apple');
+// fruits.splice(2, 3 ,);
+// fruits.pop();
+// fruits.push('Watermelon');
+// fruits.push('Strawberry');
+// fruits.splice(2 , 2);
+// fruits.splice(2 , 0 , 'fruit 1' , 'fruit 2');
+// fruits.splice(3 , 0 , 'fruit 3' , 'fruit 4');
+
+
+
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+const shop = ['Apple', 'Banana',  'Orange', 'Mango', 'Grape', 'Strawberry'];
+const input = document.querySelector('#input');
+const h2 = document.querySelector('h2');
+function getFruits(){
+    console.log(input.value)
+    if(shop.includes(input.value)){
+        console.log('fruit available haa..')
+        h2.innerHTML = `${input.value} is available`
+    }else{
+        console.log('fruit available nahi ha');
+        h2.innerHTML = `${input.value} is not available`
+    }
+
+
+    input.value = ''
+}
